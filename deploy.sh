fd3d91043fb5088fcbcefc238c809bad89fe2d75
catherine-00 <catherine_biz@daum.net>
2025-04-11 09:59:48 +0900

    Auto commit on 2025-04-11 09:59:48

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
-git add .
-git commit -m "updated:1"
-git push
+#!/bin/bash
+
+# 현재 날짜와 시간 가져오기
+now=$(date "+%Y-%m-%d %H:%M:%S")
+# # Git 명령어 실행
+ git add .
+ git commit -m "Auto commit on $now"
+ git push
+#
